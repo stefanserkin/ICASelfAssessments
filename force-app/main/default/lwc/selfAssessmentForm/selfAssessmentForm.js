23a1a65cd8a200a5d2db0127821ef3667d52dbe9
Stefan Serkin <stefanserkin@gmail.com>
2024-03-02 23:48:08 -0500

    Added submit action

--- force-app/main/default/lwc/selfAssessmentForm/selfAssessmentForm.js
+++ force-app/main/default/lwc/selfAssessmentForm/selfAssessmentForm.js
@@ -1,12 +1,15 @@
 import { LightningElement, api, wire } from 'lwc';
+import { RefreshEvent } from 'lightning/refresh';
 import getSelfAssessment from '@salesforce/apex/SelfAssessmentController.getSelfAssessment';
+import submitSelfAssessment from '@salesforce/apex/SelfAssessmentController.submitSelfAssessment';
 
 export default class SelfAssessmentForm extends LightningElement {
     @api recordId;
-    @api cardTitle = 'Self-Assessment Form';
-    @api cardIconName = 'custom:custom18';
+    @api cardTitle;
+    @api cardIconName;
 
     isLoading = false;
+    isSubmitted = false;
     error;
 
     wiredAssessment = [];
@@ -19,6 +22,7 @@ export default class SelfAssessmentForm extends LightningElement {
         this.wiredAssessment = result;
         if (result.data) {
             this.assessment = JSON.parse(JSON.stringify(result.data));
+            this.isSubmitted = this.assessment.status == 'Completed';
             this.answers = this.assessment.answers;
             this.answers.forEach(ans => {
                 ans.isTextArea = ans.questionType == 'Text Area';
@@ -56,6 +60,39 @@ export default class SelfAssessmentForm extends LightningElement {
             } else if (this.answers[answerIndex].questionType === 'Text Area') {
                 this.answers[answerIndex].answerText = selectedValue;
             }
+        }
+    }
+
+    handleSubmit() {
+        console.log(':::: handle submit');
+        console.log(':::: answers --> ' );
+        console.table(this.answers);
+        this.isLoading = true;
+
+        // Is this helpful? Does it validate fields in child component?
+        const allValid = [...this.template.querySelectorAll('lightning-input')]
+            .reduce((validSoFar, inputFields) => {
+                inputFields.reportValidity();
+                return validSoFar && inputFields.checkValidity();
+            }, true);
+        
+        if (allValid) {
+            const request = JSON.stringify(this.answers);
+            submitSelfAssessment({
+                selfAssessmentId: this.recordId, 
+                jsonAnswerString: request
+            })
+                .then((result) => {
+                    console.log('::: result --> ', result);
+                    this.isSubmitted = true;
+                    this.isLoading = false;
+                    this.dispatchEvent(new RefreshEvent());
+                })
+                .catch((error) => {
+                    this.error = error;
+                    console.error(this.error);
+                    this.isLoading = false;
+                });
         }
     }
 
